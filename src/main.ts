c5f8edbd5f31d630963e72c9061d76ea4201d35e
ItsChrisHickman <itschrishickman@gmail.com>
2025-01-23 08:47:38 -0600

    Cannot use import statement outside of module :-(

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import '@matterport/webcomponent';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Matterport WebComponent</h1>
     <div class="card">
-      <matterport-viewer m="SxQL3iGyoDo" application-key="yxszifc05b1bidcsqfr60806d"></matterport-viewer>
+      <matterport-viewer m="SxQL3iGyoDo" application-key="yxszifc05b1bidcsqfr60806d" asset-base="assets"></matterport-viewer>
     </div>
   </div>
 `;
+
+const viewer = document.querySelector('matterport-viewer');
+if (viewer !== null) {
+  viewer.addEventListener('mpSdkPlaying', evt => {
+    const mpSdk = evt.detail.mpSdk;
+    mpSdk.Camera.rotate(90, 0);
+  })
+}

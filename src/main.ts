edd76b6944c463367f1ef79edd12bf7b1c1777da
ItsChrisHickman <itschrishickman@gmail.com>
2025-01-23 10:48:45 -0600

    Setup WebComponent with asset-base and application-key

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import type { MpSdk } from '../assets/sdk.d.ts'
 
 const init = async () => {
   await import('@matterport/webcomponent');
-  const currentContainer = document.querySelector('matterport-viewer');
-  if (currentContainer === null) {
+  
+  const container = document.getElementById('app');
+  if (container === null) {
     return;
   }
-  const newWebComponent = document.createElement('matterport-viewer');
-  if (newWebComponent === null) {
+  const showcase = document.createElement('matterport-viewer');
+  if (showcase === null) {
     return;
   }
-
-  currentContainer.appendChild(newWebComponent);
-
-  newWebComponent?.addEventListener('mpSdkPlaying', async (evt: any) => {
+  showcase.setAttribute('asset-base', 'assets');
+  showcase.setAttribute('application-key','yxszifc05b1bidcsqfr60806d');
+  container.appendChild(showcase);
+  showcase?.addEventListener('mpSdkPlaying', async (evt: any) => {
     const mpSdk: MpSdk = evt.detail.mpSdk;
     mpSdk.Camera.rotate(90, 0);
   });

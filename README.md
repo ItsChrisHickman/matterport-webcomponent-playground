96a283436d498ce7148096ce629f6354a0d950b9
Chris Hickman <itschrishickman@gmail.com>
2025-01-24 09:34:18 -0600

    Removed '3D Tags' (now a branch)

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A code playground that installs the Matterport WebComponent in a Vite/TS environ
 of code examples and experimenting with 'bundle' features.  Automatically initializes and installs
 the bundle, etc.
 
-Includes a demo that creates '3D Tags' using a custom Scene Component.
-
 Try it, fork it, play with it at: 
 https://stackblitz.com/~/github.com/ItsChrisHickman/matterport-webcomponent-playground?file=src/main.ts
 
+---
+
+Explore the branches in this repo to see other demos that we've used this to build!
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import './style.css';
-import type { MpSdk } from '../public/assets/sdk'
+import type { MpSdk } from '../public/assets/sdk';
 import('@matterport/webcomponent');
-import { tagComponentFactory } from "./components/TagComponent.js";
-
 
 const init = async () => {
   // Attempt to dynamically load the webcomponent to avoid bundle errors
@@ -23,80 +21,13 @@ const init = async () => {
     const mpSdk: MpSdk = evt.detail.mpSdk;
     onShowcaseLoaded(mpSdk);
   });
-}
+};
 init();
 
 const onShowcaseLoaded = async (sdk: MpSdk) => {
-
-  // register components
-  await sdk.Scene.registerComponents([
-    {
-      name: "tag",
-      factory: tagComponentFactory(),
-    },
-  ]);
-
-  const [sceneObject] = await sdk.Scene.createObjects(1);
-
-  let lights = sceneObject.addNode();
-  lights.addComponent("mp.directionalLight", {});
-  lights.addComponent("mp.ambientLight", { intensity: 4 });
-
-  const tagNodes = new Map();
-  let counter = 0;
-  sdk.Tag.data.subscribe({
-    onAdded: function (index, item) {
-      const node = sceneObject.addNode();
-
-      sdk.Tag.editStem(index, { stemVisible: false });
-      let randomColor =
-        "#" +
-        Math.floor(Math.random() * 0xffffff)
-          .toString(16)
-          .padStart(6, "0");
-
-      let component = node.addComponent("tag", {
-        tagId: index,
-        shape: counter % 3 === 0 ? "sphere" : "box",
-        color1: "black",
-        color2: randomColor,
-        hoverColor1: randomColor,
-        hoverColor2: "black",
-      });
-      node.position.set(
-        item.anchorPosition.x + item.stemVector.x,
-        item.anchorPosition.y + item.stemVector.y,
-        item.anchorPosition.z + item.stemVector.z
-      );
-      const tagClickEmitPath = sceneObject.addPath({
-        // Arbitrary ID
-        id: `click-emit-${index}`,
-        // We're adding a path to an EMIT
-        type: sdk.Scene.PathType.EMIT,
-        node,
-        component,
-        // We're making a path to this.notify('TAG.CLICK', payload)
-        property: "TAG.CLICK",
-      });
-      sceneObject.spyOnEvent({
-        path: tagClickEmitPath,
-        // TagComponent's notify event returns the tagId:
-        // this.notify('TAG.CLICK', tagId)
-        onEvent: (tagId: string) => {
-          sdk.Mattertag.navigateToTag(tagId, sdk.Mattertag.Transition.FLY);
-        },
-      });
-      node.start();
-      tagNodes.set(item.id, node);
-    },
-    onRemoved: function (index) {
-      const node = tagNodes.get(index);
-      if (node) {
-        node.stop();
-      }
-    },
-    onCollectionUpdated: function (collection) {
-      console.log("All done!", collection);
-    },
-  });
-}
+  console.log(sdk);
+  await sdk.App.state.waitUntil(
+    (state) => state.phase === sdk.App.Phase.PLAYING
+  );
+  sdk.Camera.rotate(115, 0);
+};
